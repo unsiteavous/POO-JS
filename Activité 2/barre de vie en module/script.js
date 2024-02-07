74bd5b3dbe6181f15d30bc2a9ae36bbb55262718
Théophile Bellintani <theophile@captp.fr>
2024-02-07 10:47:25 +0100

    Correction activité 2 en JS module

---
+++ Activité 2/barre de vie en module/script.js
@@ -0,0 +1,16 @@
+import BarreDeVie from "./BarreDeVie.js";
+// On prépare le tableau qui contiendra toutes nos barres de vies.
+const tableau = []
+// On instancie ensuite notre classe.
+tableau.push(new BarreDeVie());
+
+// On lit le tableau, pour retrouver toutes les barres :
+tableau.forEach(barre => {
+  barre.boutonPerteVie.addEventListener('click', () => {
+    barre.perdreVie(10);
+  })
+
+  barre.boutonGainVie.addEventListener('click', () => {
+    barre.gagnerVie(10);
+  })
+})
---
+++ découverte/decouverte-POO.js
@@ -0,0 +1,62 @@
+window.addEventListener("DOMContentLoaded", setup);
+
+function setup(){
+  // instanciation d'un objet logement de type "maison" grâce à l'opérateur "new"
+  // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/new
+  let logement = new Logement("maison");
+  console.log(logement.getType());
+
+  // changement de la propriété type du logement
+  logement.setType("appartement");
+  console.log(logement.getType());
+
+  // instanciation d'un objet appartement avec la propriété étage = 3
+  let appartement = new Appartement(3);
+  console.log(appartement.getType() + " - " + appartement.getEtage());
+}
+
+// CLASSE : modèle pour un objet dans le code, qui permet de construire plusieurs objets du même type (appelés instances de la même classe) 
+class Logement {
+  // Underscore = c'est une variable privée
+  #type;
+
+  // CONSTRUCTEUR : est appelé quand on crée une nouvelle instance de cette classe avec le mot clé "new" 
+  constructor(type){
+    // Le mot clé "this" fait référence à la nouvelle instance, et utilise la notation dot pour attribuer la valeur reçue (type) à la clé correspondante (this.#type)
+    // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/this
+    this.#type = type;
+  }
+
+  // GET : méthode qui permet d'accèder à la valeur de la propriété depuis l'extérieur de l'objet
+  getType(){
+    return this.#type;
+  }
+
+  // SET : méthode qui permet de changer la valeur de la propriété depuis l'extérieur de l'objet
+  setType(type){
+    this.#type = type;
+  }
+
+}
+
+// HERITAGE : l'appartement hérite des mêmes propriétés qu'un logement, tout en ayant ses spécificités (avoir un étage par exemple)
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Classes/extends
+class Appartement extends Logement {
+  #etage;
+
+  constructor(etage){
+    // Pour que le constructeur du parent logement soit appelé, on utilise le mot clef "super"
+    // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/super
+    super("appartement");
+    this.#etage = etage;
+  }
+
+  getEtage(){
+    return this.#etage;
+  }
+
+  setEtage(etage){
+    this.#etage = etage;
+  }
+
+}

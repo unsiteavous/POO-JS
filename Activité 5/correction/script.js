c1b86c15f7239b046ceea175b341568c9508fff5
Théophile Bellintani <theophile@captp.fr>
2024-12-19 12:26:41 +0100

    Suppression des fichiers inutiles

--- Activité 5/correction/script.js
+++ Activité 5/correction/script.js
@@ -1,32 +1,4 @@
 import zoneDeToasts from "./JS/ZoneDeToasts.js";
-import ErrorToast from "./JS/ErrorToast.js";
-import SuccessToast from "./JS/SuccessToast.js";
-import InfoToast from "./JS/InfoToast.js";
-import WarningToast from "./JS/WarningToast.js";
 
 zoneDeToasts.init();
 
-// setTimeout(() => {
-//   let toast = new SuccessToast("Tu as réussi ton premier toast !");
-// }, 1000);
-// setTimeout(() => {
-//   let toast1 = new ErrorToast("tu n'as pas réussi");
-// }, 2000);
-// setTimeout(() => {
-//   let toast2 = new InfoToast('Ceci est un toast d\'information');
-
-// }, 3000);
-// setTimeout(() => {
-//   let toast3 = new WarningToast('Quelque chose s\'est mal passé');
-
-// }, 4000);
-
-let toast9 = new WarningToast('Quelque chose s\'est mal passé');
-let toast7 = new WarningToast('Quelque chose s\'est mal passé');
-let toast8 = new ErrorToast('Quelque chose s\'est mal passé');  
-let toast1 = new WarningToast('Quelque chose s\'est mal passé');  
-let toast2 = new InfoToast('Quelque chose s\'est mal passé');  
-let toast3 = new WarningToast('Quelque chose s\'est mal passé');  
-let toast4 = new ErrorToast('Quelque chose s\'est mal passé');  
-let toast5 = new SuccessToast('Quelque chose s\'est mal passé');  
-let toast6 = new WarningToast('Quelque chose s\'est mal passé');  
